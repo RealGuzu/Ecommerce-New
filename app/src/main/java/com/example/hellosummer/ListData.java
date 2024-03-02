f621fc0ea15f4ae382337435811be488ad122c0b
RealGuzu <147645744+RealGuzu@users.noreply.github.com>
2024-03-02 10:20:29 -0500

    List Adapter

--- app/src/main/java/com/example/hellosummer/ListData.java
+++ app/src/main/java/com/example/hellosummer/ListData.java
@@ -2,12 +2,12 @@ package com.example.hellosummer;
 
 public class ListData {
 
-    String name, time;
+    String name, cost;
     int ingredients, desc;
     int image;
-    public ListData(String name, String time, int ingredients, int desc, int image) {
+    public ListData(String name, String cost, int ingredients, int desc, int image) {
         this.name = name;
-        this.time = time;
+        this.cost = cost;
         this.ingredients = ingredients;
         this.desc = desc;
         this.image = image;
